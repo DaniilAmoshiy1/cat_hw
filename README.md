bc5def221d1620a02242bd67563f89671227344d
DaniilAmoshiy1 <daniil13022007god@gmail.com>
2024-10-12 17:18:59 +0400

    major update: web page is fully structured.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cat_hw
-This is my home work, in this repository will be html code, css and JS.
+# Cat Home Work
+## This is my homework code, I touched on HTML and JavaScript here, the page shows a cat and an input field for setting the interval for the appearance of a new picture and a refresh counter.
+
+### You can go to the web page: (Will be after publication on Git Hub)
---
+++ cat-updater.js
@@ -0,0 +1,40 @@
+const intervalInput = document.getElementById('intervalInput');
+const catImage = document.getElementById('catImage');
+const catCounter = document.getElementById('catCounter');
+const errorMessage = document.getElementById('errorMessage');
+
+let updateInterval;
+let catCount = 0;
+
+
+function updateCatImage() {
+    catImage.src = `https://cataas.com/cat?t=${new Date().getTime()}`;
+    catCount++;
+    catCounter.textContent = catCount;
+}
+
+function setUpdateInterval() {
+    if (updateInterval) {
+        clearInterval(updateInterval);
+    }
+
+    const interval = intervalInput.value;
+
+    if (interval > 0) {
+        updateInterval = setInterval(updateCatImage, interval * 1000);
+        console.log(`Интервал обновления установлен на ${interval} секунд`);
+        errorMessage.style.display = 'none';
+    } else {
+        console.log('Пожалуйста, введите положительное число секунд');
+        errorMessage.style.display = 'block';
+    }
+}
+
+intervalInput.addEventListener('input', function(e) {
+    if (e.target.value.startsWith('-')) {
+        e.target.value = '';
+    }
+    setUpdateInterval();
+});
+
+updateCatImage();
